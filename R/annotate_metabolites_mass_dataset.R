04f6ca49fa0887c984767a26cdd1ebf455c8fac8
Heather Kates <hkates@ufl.edu>
2023-01-30 13:28:08 -0500

    Add rp_custom arg

--- R/annotate_metabolites_mass_dataset.R
+++ R/annotate_metabolites_mass_dataset.R
@@ -92,7 +92,7 @@ annotate_metabolites_mass_dataset <-
            rt.match.tol = 30,
            polarity = c("positive", "negative"),
            ce = "all",
-           column = c("rp", "hilic"),
+           column = c("rp", "hilic","rp_custom"),
            ms1.match.weight = 0.25,
            rt.match.weight = 0.25,
            ms2.match.weight = 0.5,
